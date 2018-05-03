8c9f5c74a36ab4963470eb7024b303b6eb004e67
Neil Clayton <neil@cloudnine.net.nz>
2018-05-04 10:22:42 +1200

    initial commit

--- StrongNamingNightmare/Program.cs
+++ StrongNamingNightmare/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +15,10 @@ namespace StrongNamingNightmare
         [STAThread]
         static void Main()
         {
+            Assembly foo = Assembly.Load("Npgsql.EntityFrameworkCore.PostgreSQL, Version=2.0.2.0, Culture=neutral, PublicKeyToken=5d8b90d52f46fda7");
+            Console.WriteLine("Got " + foo);
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void CanWeLoadTheThing()
+        {
+            Assembly foo = Assembly.Load("Npgsql.EntityFrameworkCore.PostgreSQL, Version=2.0.2.0, Culture=neutral, PublicKeyToken=5d8b90d52f46fda7");
+            Console.WriteLine("Got " + foo);
+        }
+    }
+}
